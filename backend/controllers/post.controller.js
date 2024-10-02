d3f8e878c412def4ce3520bd2d1de34730da9802
keithi16 <keithi@digitalcare.com.br>
2024-10-02 07:34:30 -0300

    add: send cmment notification email

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,12 +1,12 @@
+import cloudinary from '../lib/cloudinary.js';
 import Post from '../models/post.model.js';
 import Notification from '../models/notification.model.js';
-
-import cloudinary from '../lib/cloudinary.js';
+import { sendCommentNotificationEmail } from '../emails/emailHandlers.js';
 
 export const getFeedPosts = async (req, res) => {
 	try {
 		const posts = await Post.find({
-			author: { $in: req.user.connections },
+			author: { $in: [...req.user.connections, req.user._id] },
 		})
 			.populate('author', 'name username profilePicture headline')
 			.populate('comments.user', 'name profilePicture')
@@ -22,12 +22,10 @@ export const getFeedPosts = async (req, res) => {
 export const createPost = async (req, res) => {
 	try {
 		const { content, image } = req.body;
-
 		let newPost;
 
 		if (image) {
 			const imgResult = await cloudinary.uploader.upload(image);
-
 			newPost = new Post({
 				author: req.user._id,
 				content,
@@ -57,7 +55,7 @@ export const deletePost = async (req, res) => {
 		const post = await Post.findById(postId);
 
 		if (!post) {
-			return res.status(400).json({ message: 'Post not found' });
+			return res.status(404).json({ message: 'Post not found' });
 		}
 
 		// check if the current user is the author of the post
@@ -67,7 +65,7 @@ export const deletePost = async (req, res) => {
 				.json({ message: 'You are not authorized to delete this post' });
 		}
 
-		// delete the image from cloudinary as well
+		// delete the image from cloudinary as well!
 		if (post.image) {
 			await cloudinary.uploader.destroy(
 				post.image.split('/').pop().split('.')[0],
@@ -75,9 +73,10 @@ export const deletePost = async (req, res) => {
 		}
 
 		await Post.findByIdAndDelete(postId);
+
 		res.status(200).json({ message: 'Post deleted successfully' });
 	} catch (error) {
-		console.error('Error in deletePost controller:', error);
+		console.log('Error in delete post controller', error.message);
 		res.status(500).json({ message: 'Server error' });
 	}
 };
@@ -110,7 +109,7 @@ export const createComment = async (req, res) => {
 		).populate('author', 'name email username headline profilePicture');
 
 		// create a notification if the comment owner is not the post owner
-		if (post.author.toString() !== req.user._id.toString()) {
+		if (post.author._id.toString() !== req.user._id.toString()) {
 			const newNotification = new Notification({
 				recipient: post.author,
 				type: 'comment',
@@ -119,11 +118,61 @@ export const createComment = async (req, res) => {
 			});
 
 			await newNotification.save();
+
+			try {
+				const postUrl = `${process.env.CLIENT_URL}/post/${postId}`;
+				await sendCommentNotificationEmail(
+					post.author.email,
+					post.author.name,
+					req.user.name,
+					postUrl,
+					content,
+				);
+			} catch (error) {
+				console.log('Error in sending comment notification email:', error);
+			}
 		}
 
 		res.status(200).json(post);
 	} catch (error) {
 		console.error('Error in createComment controller:', error);
+		res.status(500).json({ message: 'Server error' });
+	}
+};
+
+export const likePost = async (req, res) => {
+	try {
+		const postId = req.params.id;
+		const post = await Post.findById(postId);
+		const userId = req.user._id;
+
+		if (post.likes.includes(userId)) {
+			// unlike the post
+			post.likes = post.likes.filter(
+				(id) => id.toString() !== userId.toString(),
+			);
+		} else {
+			//like the post
+			post.likes.push(userId);
+
+			//create a notification if the post owner is not the user who liked
+			if (post.author.toString() !== userId.toString()) {
+				const newNotification = new Notification({
+					recipient: post.author,
+					type: 'like',
+					relatedUser: userId,
+					relatedPost: postId,
+				});
+
+				await newNotification.save();
+			}
+		}
+
+		await post.save();
+
+		res.status(200).json(post);
+	} catch (error) {
+		console.error('Error in likePost controller:', error);
 		res.status(500).json({ message: 'Server error' });
 	}
 };
--- backend/emails/emailHandlers.js
+++ backend/emails/emailHandlers.js
@@ -19,3 +19,32 @@ export const sendWelcomeEmail = async (email, name, profileUrl) => {
 		throw error;
 	}
 };
+
+export const sendCommentNotificationEmail = async (
+	recipientEmail,
+	recipientName,
+	commenterName,
+	postUrl,
+	commentContent,
+) => {
+	const recipient = [{ email: recipientEmail }];
+
+	try {
+		const response = await mailtrapClient.send({
+			from: sender,
+			to: recipient,
+			subject: 'New Comment on Your Post',
+			html: createCommentNotificationEmailTemplate(
+				recipientName,
+				commenterName,
+				postUrl,
+				commentContent,
+			),
+			category: 'comment_notification',
+		});
+		console.log('Comment Notification Email sent successfully', response);
+	} catch (error) {
+		// biome-ignore lint/complexity/noUselessCatch: <explanation>
+		throw error;
+	}
+};
